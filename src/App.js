8f3bf693a9fd6c9bb0bc5aae142fa9121e0945a9
Mark Jerly Bundalian <mjbundalian61@gmail.com>
2021-10-02 14:07:33 +0800

    Add search UI

--- src/App.js
+++ src/App.js
@@ -1,37 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect } from 'react';
 import './App.css';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-    };
-  }
+function App() {
+    const address = "0x88A14AF453b14070B9B943eea32bf3F534dFa01a"
+    const urlETH = `https://api-ropsten.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10&sort="desc"&apikey=${process.env.REACT_APP_ROPSTEN_API_KEY}`
+    const urlERC20 = `https://api-ropsten.etherscan.io/api?module=account&action=tokentx&address=${address}&startblock=0&endblock=99999999&page=1&offset=10&sort="desc"&apikey=${process.env.REACT_APP_ROPSTEN_API_KEY}`
 
-  componentWillMount() {
-  }
+    const fetchRopstenTxs = async () => {
+        let data
 
-  render() {
+        await axios.get(urlETH).then(res => {
+            data = res.data
+        })
+
+        return data
+    }
+    const fetchRopstenERC20Txs = async () => {
+        let data
+
+        await axios.get(urlERC20).then(res => {
+            data = res.data
+        })
+
+        return data
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+        <div className="app h-100">
+            <div className="container h-100">
+                <div className="d-flex justify-content-center align-items-center h-100">
+                    <div className="app-content text-center">
+                        <h2 className="font-bold mb-1 light-black">Ropsten Test Network Explorer</h2>
+                        <p className="font-thin font-italic mb-4 light-grey">Check out the transactions on the Ropsten Test Network by any given address</p>
+                        <form>
+                            <div className="d-flex align-items-center w-100">
+                                <input type="text" className="app-input w-100" placeholder="Enter address" />
+                                <button className="app-btn" type="submit">SEARCH</button>
+                            </div>
+                        </form>
+                    </div>    
+                </div>                
+            </div>
+        </div>
+    )
 }
 
 export default App;
